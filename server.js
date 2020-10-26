3af8ae4d185e242b6d188db6a9724b5427141add
km4891 <kmorarity@gmail.com>
2020-10-25 19:44:02 -0700

    up and running

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
-const cTable = require('console.table');
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const cTable = require("console.table");
 
 const connection = mysql.createConnection({
     host: "localhost",
 
-    // Your port
-    port: 3001,
-
+   
     // Your username
     user: "root",
 
@@ -39,10 +37,10 @@ function startScreen() {
                     addDepartment();
                     break;
                 case "Add role":
-                    viewDepartment();
+                    addRole();
                     break;
                 case "Add employee":
-                    viewDepartment();
+                    addEmployee();
                     break;
                 case "View departments":
                     viewDepartment();
@@ -54,43 +52,166 @@ function startScreen() {
                     viewEmployees();
                     break;
                 case "Update employee role":
-                    viewDepartment();
+                    updateEmployee();
                     break;
                 default:
                     quit()
-
             }
-
-
         });
+}
+
+function addRole() {
+    
+
+    inquirer.prompt(
+        [{
+        type: "input",
+        message: "What's the name of the role?",
+        name: "roleName"
+    },
+    {
+        type: "input",
+        message: "What is the salary for this role?",
+        name: "salaryTotal"
+    },
+    {
+        type: "input",
+        message: "What is the department id number?",
+        name: "deptID"
+    }
+    ]
+    ).then(function(answer){
+        let query = `INSERT INTO role (title, salary, department_id) VALUES ("${answer.roleName}", "${answer.salaryTotal}", ${answer.deptID})`
+        
+        connection.query(query, function(err, res) {
+            if (err) throw err;
+            console.table(res)
+            startScreen()
+    })
+
+
+    })
 
 }
 
-function addDepartment() {
+function addEmployee() {
     
-    let query = "SELECT * FROM ee_info_db WHERE deptName=?"
 
-    inquirer.prompt({
+    inquirer.prompt(
+        [{
+        type: "input",
+        message: "What's the first name of the employee?",
+        name: "eeFirstName"
+    },
+    {
+        type: "input",
+        message: "What's the last name of the employee?",
+        name: "eeLastName"
+    },
+    {
+        type: "input",
+        message: "What is the employee's role id number?",
+        name: "roleID"
+    },
+    {
+        type: "input",
+        message: "What is the manager id number?",
+        name: "managerID"
+    }
+    ]
+    ).then(function(answer){
+        let query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answer.eeFirstName}", "${answer.eeLastName}", ${answer.roleID},${answer.managerID})`
+        
+        connection.query(query, function(err, res) {
+            if (err) throw err;
+            console.table(res)
+            startScreen()
+    })
+
+
+    })
+
+}
+
+function updateEmployee() {
+    
+
+    inquirer.prompt(
+        [
+        {
+      
+        type: "list",
+        message: "Which employee would you like to update?",
+        choices: ["John", "Jill", "Jack", "Steve"],
+        name: "eeUpdate"
+      
+    },
+
+            {
       
         type: "input",
-        message: "What is the name of the department?",
-        name: "deptName"
+        message: "What do you want to update to?",
+        name: "updateRole"
       
+    }
+        ]
+    ).then(function(answer){
+        // let query = `INSERT INTO department (name) VALUES ("${answer.deptName}")`
+        let query = `'UPDATE employee SET role_id="updateRole" WHERE ${answer.eeUpdate}`
+        
+        connection.query(query, function(err, res) {
+            if (err) throw err;
+            console.table(res)
+            startScreen()
     })
-    connection.query(query, function(err, res) {
+
+
+    })
+
+}
+
+function viewDepartment() {
+    // select from the db
+    let query = "SELECT * FROM department"
+    connection.query(query, function (err, res) {
         if (err) throw err;
         console.table(res)
         startScreen()
 
+    })
+}
+
+function viewRoles() {
+    // select from the db
+    let query = "SELECT * FROM role"
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res)
+        startScreen()
+
+    })
+
+}
+
+function viewEmployees() {
+    // select from the db
+    let query = "SELECT * FROM employee"
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res)
+        startScreen()
 
     })
 
 }
 
 
-function quit() {
 
+
+
+
+
+function quit() {
     connection.end()
     process.exit()
-
 }
